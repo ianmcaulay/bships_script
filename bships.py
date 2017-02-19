0cfa19f7654b20276c52ea081cd8d885395caec8
Ian McAulay <mcaulay.ian@gmail.com>
2017-02-19 01:08:04 -0800

    fixing bugs

--- bships.py
+++ bships.py
@@ -2,9 +2,9 @@ import urllib.request
 
 def pull_number():
 	url = "http://makemehost.com/games.php"
-	content = urllib.request.urlopen(url)
+	content = str(urllib.request.urlopen(url))
 	search_string = "Battleships Pro 4v4"
-	bships_index = content.find(bships_string)
+	bships_index = content.find(search_string)
 	bships_string = content[bships_index: bships_index + 50]
 	ingame_index = bships_string.find("/8")
 	return bships_string[ingame_index - 1]
