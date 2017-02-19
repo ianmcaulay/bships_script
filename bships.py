75c97fdaba8a3e64885edd5c7b05966402b934bf
Ian McAulay <mcaulay.ian@gmail.com>
2017-02-19 04:06:11 -0800

    Only prints when players change

--- bships.py
+++ bships.py
@@ -4,8 +4,11 @@ import winsound
 
 def pull_number():
 	url = "http://makemehost.com/games.php"
-	content = str(urllib.request.urlopen(url).read())
-	search_string = "Battleships Pro 4v4"
+	#url = "https://entgaming.net/forum/games.php"
+	req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+	#content = str(urllib.request.urlopen(url, headers={'User-Agent' : "Magic Browser"}).read())
+	content = str(urllib.request.urlopen(req).read())
+	search_string = "Battleships Pro"
 	bships_index = content.find(search_string)
 	bships_string = content[bships_index: bships_index + 50]
 	ingame_index = bships_string.find("/8")
@@ -14,10 +17,13 @@ def pull_number():
 
 def start_checking_num_players():
 	alert_threshold = 8
-	refresh_time = 5
+	refresh_time = 1
+	old_val = -1
 	while True:
 		num_players = pull_number()
-		print("Current Number of Players: " + str(num_players))
+		if num_players != old_val:
+			print("Current Number of Players: " + str(num_players))
+			old_val = num_players
 		if num_players >= alert_threshold:
 			
 			winsound.PlaySound('bships_alert.wav', winsound.SND_FILENAME)
